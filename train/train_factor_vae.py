aafa0eed92d738dce215577cbc48b90e05f1f31a
Irène Burger <ireneburger@macbook-air-de-irene.home>
2021-03-24 12:15:12 +0100

    fixes + factorvae inherits betavae

--- train/train_factor_vae.py
+++ train/train_factor_vae.py
@@ -45,20 +45,21 @@ def train_factor_VAE(model, discriminator, vae_optimizer, discriminator_optimize
         train_tc_loss = 0
         train_d_loss = 0
         for data1, data2 in train_loader:
+            batch_size = data1.size(0)
             x_recon1, mu1, logvar1, z1 = model(data1)
 
             recon_l = recon_loss(x_recon1, data1)
-            train_recon_loss+= recon_l* data1.size(0)
+            train_recon_loss+= recon_l* batch_size
 
             kld_l = kld_loss(mu1, logvar1)
-            train_kld_loss+= kld_l* data1.size(0)
+            train_kld_loss+= kld_l* batch_size
 
             dz = discriminator(z1)
             tc_l = tc_loss(dz)
-            train_tc_loss+= tc_l* data1.size(0)
+            train_tc_loss+= tc_l* batch_size
 
             total_loss = recon_l + kld_l + torch.mul(gamma, tc_l)
-            train_vae_loss += total_loss * data1.size(0)
+            train_vae_loss += total_loss * batch_size
 
             vae_optimizer.zero_grad()
             total_loss.backward(retain_graph=True)
@@ -71,7 +72,7 @@ def train_factor_VAE(model, discriminator, vae_optimizer, discriminator_optimize
             dnewz = discriminator(newz)
             dz = discriminator(z1.detach())
             discriminator_l = 0.5*(discriminator_loss(dnewz,True) + discriminator_loss(dz,False))
-            train_d_loss += discriminator_l * data1.size(0)
+            train_d_loss += discriminator_l * batch_size
 
             discriminator_optimizer.zero_grad()
             discriminator_l.backward()
@@ -79,17 +80,17 @@ def train_factor_VAE(model, discriminator, vae_optimizer, discriminator_optimize
 
             count+=1
  
-            vae_loss.append(train_vae_loss.item()/len(train_loader.dataset))
-    
+        vae_loss.append(train_vae_loss.item()/len(train_loader.dataset))
+   
         if viz:
-            print("Epoch "+str(epoch)+":")
+            print("Epoch "+str(epoch+1)+":")
             print("training vae loss of "+ str(train_vae_loss.item()/len(train_loader.dataset)))
             print("training recon loss of "+ str(train_recon_loss.item()/len(train_loader.dataset)))
             print("training kld loss of "+ str(train_kld_loss.item()/len(train_loader.dataset)))
             print("training TC loss of "+ str(train_tc_loss.item()/len(train_loader.dataset)))
             print("training discriminator loss of "+ str(train_d_loss.item()/len(train_loader.dataset)))
     if viz:
-        plt.plot(np.arange(1,nb_epoch+1,1), train_vae_losses)
+        plt.plot(np.arange(1,nb_epoch+1,1), vae_loss)
         plt.xlabel("Epochs")
         plt.ylabel("Loss")
         plt.title("Train VAE loss evolution over epochs")
