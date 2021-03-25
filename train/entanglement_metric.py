253fba5ec65c5f67956aea8baf2e769a41c04795
Irène Burger <ireneburger@macbook-air-de-irene.home>
2021-03-25 16:20:22 +0100

    autopush

--- train/entanglement_metric.py
+++ train/entanglement_metric.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
 
 def compute_latent_variance(model, dataset, size = 10000, device = None):
@@ -6,52 +7,55 @@ def compute_latent_variance(model, dataset, size = 10000, device = None):
     imgs_sampled = dataset.simulate_images(size)
     latents = []
     loader = DataLoader(imgs_sampled, batch_size=64)
-    
+
     for data in loader:
         data = data.float()
-        
+
         if device != None:
             data = data.to(device)
-            
+
         _, _, _, z = model(data)
         z = list(z.detach().cpu().numpy())
         latents= latents+[list(l) for l in z]
-        
+
     global_vars = np.var(latents, axis = 0) 
     return global_vars
 
 def entanglement_metric_factor_vae(model, dataset, nb_samples, sample_size, device = None):
-    
+    model.eval()
+
     loss = 0
     factors_nb = dataset.num_factors()
     classification = np.zeros((factors_nb,10))
-    global_vars = compute_latent_variance(model, dataset, device = device)
     
-    for i in range(nb_samples):
+    with torch.no_grad():
         
-        k = np.random.randint(factors_nb-1)+1
-        imgs_sampled = dataset.simulate_images(sample_size, fixed_factor=k)
-        loader = DataLoader(imgs_sampled, batch_size=64)
-        latents_rep = []
-        
-        for data in loader:
-            
-            data = data.float()
-            
-            if device != None:
-                data = data.to(device)
-                
-            _, _, _, z = model(data)
-            z = list(z.detach().cpu().numpy())
-            latents_rep= latents_rep+[list(l) for l in z]
-        latents_var = np.var(latents_rep, axis = 0) 
-        latents_var_normalized = np.divide(latents_var, global_vars)
-        for i in range(10):
-            if global_vars[i] >=1:
-                latents_var_normalized[i] = np.inf
-                
-        idx = np.argmin(latents_var_normalized)
-        classification[k,idx]+=1
+        global_vars = compute_latent_variance(model, dataset, device = device)
+    
+        for i in range(nb_samples):
+
+            k = np.random.randint(factors_nb-1)+1
+            imgs_sampled = dataset.simulate_images(sample_size, fixed_factor=k)
+            loader = DataLoader(imgs_sampled, batch_size=64)
+            latents_rep = []
+            for data in loader:
+
+                data = data.float()
+
+                if device != None:
+                    data = data.to(device)
+
+                _, _, _, z = model(data)
+                z = list(z.detach().cpu().numpy())
+                latents_rep= latents_rep+[list(l) for l in z]
+            latents_var = np.var(latents_rep, axis = 0) 
+            latents_var_normalized = np.divide(latents_var, global_vars)
+            for i in range(10):
+                if global_vars[i] >=1:
+                    latents_var_normalized[i] = np.inf
+
+            idx = np.argmin(latents_var_normalized)
+            classification[k,idx]+=1
         
     print(classification)
     for i in range(factors_nb):
