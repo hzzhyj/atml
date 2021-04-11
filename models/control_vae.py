29badb3cb831db6939d495f2aae3fe05de9234b6
Richie Lo <richielyl@hotmail.com>
2021-04-11 21:52:19 +0100

    bernoulli cvae

--- models/control_vae.py
+++ models/control_vae.py
@@ -55,3 +55,29 @@ class ControlVAEDSprites(BetaVAEDSprites):
         # Get new beta
         beta, _ = self.beta_controller.pid_update(self.C, KL)
         return beta
+
+class ControlVAEDcnn(BetaVAECelebA):
+    def __init__(self, controller_args, n_latents=10):
+        super(ControlVAEDcnn, self).__init__(n_latents)
+
+        self.init_controller(controller_args)
+
+    def init_controller(self, controller_args):
+        self.C = controller_args['C']
+        self.C_max = controller_args['C_max']
+        self.C_step_val = controller_args['C_step_val']
+        self.C_step_period = controller_args['C_step_period']
+        self.beta_controller = PIDControl(controller_args['Kp'], controller_args['Ki'], controller_args['Kd'])
+
+    def update_beta(self, iter_idx, KL):
+        # Update C
+        if((iter_idx % self.C_step_period) == 0):
+            self.C += self.C_step_val
+
+        # Threshold C
+        if self.C > self.C_max:
+            self.C = self.C_max
+
+        # Get new beta
+        beta, _ = self.beta_controller.pid_update(self.C, KL)
+        return beta
